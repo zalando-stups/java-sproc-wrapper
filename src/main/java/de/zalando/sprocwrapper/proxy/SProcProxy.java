216c50587a247bc0fa56f99f2bc14779b4c0df5f
Tom Wieczorek <tom.wieczorek@zalando.de>
2015-08-14 15:32:31 +0200

    Add support to equals/hashCode/toString for dynamic SProc proxy instances

--- src/main/java/de/zalando/sprocwrapper/proxy/SProcProxy.java
+++ src/main/java/de/zalando/sprocwrapper/proxy/SProcProxy.java
@@ -7,17 +7,21 @@ import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Objects;
+import com.google.common.reflect.AbstractInvocationHandler;
+
 import de.zalando.sprocwrapper.dsprovider.DataSourceProvider;
 
 /**
  * @author  jmussler
  */
-class SProcProxy implements java.lang.reflect.InvocationHandler {
+class SProcProxy extends AbstractInvocationHandler {
 
     private final HashMap<Method, StoredProcedure> sprocs = new HashMap<Method, StoredProcedure>();
     private final DataSourceProvider dataSourceProvider;
 
     private static final Logger LOG = LoggerFactory.getLogger(OtherStoredProcedureParameter.class);
+    private final String description;
 
     public boolean addStoredProcedure(final Method method, final StoredProcedure p) {
         if (sprocs.containsKey(method)) {
@@ -28,23 +32,33 @@ class SProcProxy implements java.lang.reflect.InvocationHandler {
         return true;
     }
 
-    public SProcProxy(final DataSourceProvider provider) {
+    public SProcProxy(final DataSourceProvider provider, final String description) {
         if (provider == null) {
             throw new IllegalArgumentException("DataSourceProvider cannot be null");
         }
 
         dataSourceProvider = provider;
+        this.description = description;
     }
 
     @Override
-    public Object invoke(final Object proxy, final Method m, final Object[] args) {
-        StoredProcedure p = sprocs.get(m);
+    protected Object handleInvocation(final Object proxy, final Method method, final Object[] args) {
+        final StoredProcedure p = sprocs.get(method);
 
         if (p == null) {
-            LOG.warn("no StoredProcedure found for method {}", m);
+            LOG.warn("no StoredProcedure found for method {}", method);
             return null;
         }
 
-        return p.execute(dataSourceProvider, new InvocationContext(proxy, m, args));
+        return p.execute(dataSourceProvider, new InvocationContext(proxy, method, args));
+    }
+
+    @Override
+    public String toString() {
+        return
+            Objects.toStringHelper(this)                          //
+                   .addValue(description)                         //
+                   .add("dataSourceProvider", dataSourceProvider) //
+                   .toString();
     }
 }
