d53b847ffbca33a03b3940ba0663fe8c739211d5
akushsky <akushsky@it-universe.ru>
2015-09-10 22:28:36 +0400

    So, get another bugs with UUID and rewrite conversion.

--- src/main/java/de/zalando/typemapper/core/fieldMapper/UUIDValueTransformer.java
+++ src/main/java/de/zalando/typemapper/core/fieldMapper/UUIDValueTransformer.java
@@ -2,14 +2,16 @@ package de.zalando.typemapper.core.fieldMapper;
 
 import de.zalando.sprocwrapper.globalvaluetransformer.annotation.GlobalValueTransformer;
 import de.zalando.typemapper.core.ValueTransformer;
+import org.postgresql.util.PGobject;
 
+import java.sql.SQLException;
 import java.util.UUID;
 
 /**
  * Created by akushsky on 03.09.2015.
  */
 @GlobalValueTransformer
-public class UUIDValueTransformer extends ValueTransformer<String, UUID> {
+public class UUIDValueTransformer extends ValueTransformer<PGobject, UUID> {
 
     @Override
     public UUID unmarshalFromDb(String value) {
@@ -17,7 +19,15 @@ public class UUIDValueTransformer extends ValueTransformer<String, UUID> {
     }
 
     @Override
-    public String marshalToDb(UUID uuid) {
-        return uuid.toString();
+    public PGobject marshalToDb(UUID uuid) {
+        PGobject pgUUID = new PGobject();
+        pgUUID.setType("uuid");
+        try {
+            pgUUID.setValue(uuid.toString());
+        } catch (SQLException e) {
+            throw new ClassCastException(e.getMessage());
+        }
+
+        return pgUUID;
     }
 }
