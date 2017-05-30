a385e6b6597db300bf54a4aae9fee72f859d8c71
Vadeg <0xvadeg@gmail.com>
2017-05-30 10:14:33 +0200

    Fix test timezone

--- src/test/java/de/zalando/typemapper/postgres/PgSerializerTest.java
+++ src/test/java/de/zalando/typemapper/postgres/PgSerializerTest.java
@@ -1,8 +1,8 @@
 package de.zalando.typemapper.postgres;
 
 import java.sql.SQLException;
-import java.time.Instant;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
@@ -43,9 +43,9 @@ public class PgSerializerTest {
             this.expectedString = null;
             this.expectedPattern = (Pattern) expected;
         } else {
-           throw new IllegalArgumentException(String.format(
-                   "Expected either a String or a Pattern, got: %s (%s)",
-                   expected, expected == null ? null : expected.getClass()));
+            throw new IllegalArgumentException(String.format(
+                    "Expected either a String or a Pattern, got: %s (%s)",
+                    expected, expected == null ? null : expected.getClass()));
         }
     }
 
@@ -58,33 +58,39 @@ public class PgSerializerTest {
     @SuppressWarnings("deprecation")
     @Parameters
     public static Collection<Object[]> generateData() throws SQLException {
+
         return Arrays.asList(
-                new Object[][] {
-                    {new Date(112, 11, 1, 6, 6, 6), Pattern.compile("2012-12-01 06:06:06[+-]?\\d{2}")},
-                    {new Date(112, 9, 1, 6, 6, 6), Pattern.compile("2012-10-01 06:06:06[+-]?\\d{2}")},
-                    {Date.from(DateTimeFormatter.ISO_OFFSET_DATE_TIME.parse("2017-05-14T12:34:56.123456789+02:00", Instant::from)), Pattern.compile("2017-05-14 12:34:56.123000[+-]?\\d{2}")},
-                    {1, "1"},
-                    {69, "69"},
-                    {true, "t"},
-                    {new int[] {1, 2, 3, 4}, "{1,2,3,4}"},
-                    {new Integer[] {null, 2, 3, 4}, "{NULL,2,3,4}"},
-                    {ARRAY("a", "b").asJdbcArray("text"), "{a,b}"},
-                    {ARRAY("a", "b(x)").asJdbcArray("text"), "{a,b(x)}"},
-                    {ARRAY("a", "b{x}").asJdbcArray("text"), "{a,\"b{x}\"}"},
-                    {
-                        ARRAY("first element", "second \"quoted\" element").asJdbcArray("text"),
-                        "{\"first element\",\"second \\\"quoted\\\" element\"}"
-                    },
-                    {ROW(1, 2), "(1,2)"},
-                    {ROW("a", "b{x}"), "(a,b{x})"},
-                    {ROW("a", "b(x)"), "(a,\"b(x)\")"},
-                    {ROW(1, 2, ARRAY("a", "b")), "(1,2,\"{a,b}\")"},
-                    {ROW("a", "b", new int[] {1, 2, 3, 4}), "(a,b,\"{1,2,3,4}\")"},
-                    {ROW("a", null, ARRAY(ROW(1), ROW(2), null)), "(a,,\"{(1),(2),NULL}\")"},
-                    {
-                        ROW("a", null, ARRAY(ROW(1, 11), ROW(2, 22), null)),
-                        "(a,,\"{\"\"(1,11)\"\",\"\"(2,22)\"\",NULL}\")"
-                    },
+                new Object[][]{
+                        {new Date(112, 11, 1, 6, 6, 6), Pattern.compile("2012-12-01 06:06:06[+-]?\\d{2}")},
+                        {new Date(112, 9, 1, 6, 6, 6), Pattern.compile("2012-10-01 06:06:06[+-]?\\d{2}")},
+                        {Date.from(
+                                LocalDateTime.of(2017, 5, 14, 12, 34, 56, 123456789)
+                                        .atZone(ZoneId.systemDefault())
+                                        .toInstant()
+                        ),
+                                Pattern.compile("2017-05-14 12:34:56.123000[+-]?\\d{2}")},
+                        {1, "1"},
+                        {69, "69"},
+                        {true, "t"},
+                        {new int[]{1, 2, 3, 4}, "{1,2,3,4}"},
+                        {new Integer[]{null, 2, 3, 4}, "{NULL,2,3,4}"},
+                        {ARRAY("a", "b").asJdbcArray("text"), "{a,b}"},
+                        {ARRAY("a", "b(x)").asJdbcArray("text"), "{a,b(x)}"},
+                        {ARRAY("a", "b{x}").asJdbcArray("text"), "{a,\"b{x}\"}"},
+                        {
+                                ARRAY("first element", "second \"quoted\" element").asJdbcArray("text"),
+                                "{\"first element\",\"second \\\"quoted\\\" element\"}"
+                        },
+                        {ROW(1, 2), "(1,2)"},
+                        {ROW("a", "b{x}"), "(a,b{x})"},
+                        {ROW("a", "b(x)"), "(a,\"b(x)\")"},
+                        {ROW(1, 2, ARRAY("a", "b")), "(1,2,\"{a,b}\")"},
+                        {ROW("a", "b", new int[]{1, 2, 3, 4}), "(a,b,\"{1,2,3,4}\")"},
+                        {ROW("a", null, ARRAY(ROW(1), ROW(2), null)), "(a,,\"{(1),(2),NULL}\")"},
+                        {
+                                ROW("a", null, ARRAY(ROW(1, 11), ROW(2, 22), null)),
+                                "(a,,\"{\"\"(1,11)\"\",\"\"(2,22)\"\",NULL}\")"
+                        },
                 });
     }
 
