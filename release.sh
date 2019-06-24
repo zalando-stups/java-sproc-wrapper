c381940e62009b6adeaaf9719f1f2bce16cc2e0e
Willi Schönborn <willi.schoenborn@zalando.de>
2019-06-24 11:16:41 +0200

    GH-55 Added relase script

---
+++ release.sh
@@ -0,0 +1,24 @@
+#!/bin/sh -ex
+
+: ${1?"Usage: $0 <[pre]major|[pre]minor|[pre]patch|prerelease>"}
+
+./mvnw scm:check-local-modification
+
+current=$(git describe --abbrev=0 || echo 0.0.0)
+release=$(semver ${current} -i $1 --preid RC)
+next=$(semver ${release} -i minor)
+
+git checkout -b release/${release}
+
+./mvnw versions:set -D newVersion=${release}
+git commit -am "Release ${release}"
+./mvnw clean deploy scm:tag -P release -D tag=${release} -D pushChanges=false
+
+./mvnw versions:set -D newVersion=${next}-SNAPSHOT
+git commit -am "Development ${next}-SNAPSHOT"
+
+git push
+git push --tags
+
+git checkout master
+git branch -D release/${release}
