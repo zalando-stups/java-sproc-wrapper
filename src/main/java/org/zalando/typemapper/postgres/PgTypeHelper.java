d3a34d7b4845febe039aca48bd546d8e4b2c42bf
Gregor Zeitlinger <gregor.zeitlinger@zalando.de>
2022-10-10 14:51:33 +0200

    backport to Java 8

--- src/main/java/org/zalando/typemapper/postgres/PgTypeHelper.java
+++ src/main/java/org/zalando/typemapper/postgres/PgTypeHelper.java
@@ -1,29 +1,14 @@
 package org.zalando.typemapper.postgres;
 
-import java.lang.reflect.Array;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.sql.Types;
-
-import java.util.*;
-
 import javax.persistence.Column;
 
+import com.google.common.base.Optional;
 import org.postgresql.core.BaseConnection;
-
 import org.postgresql.jdbc.PostgresJDBCDriverReusedTimestampUtils;
-
 import org.postgresql.util.PGobject;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import com.google.common.base.Optional;
-
-
+import org.zalando.sprocwrapper.util.NameUtils;
 import org.zalando.typemapper.annotations.DatabaseField;
 import org.zalando.typemapper.annotations.DatabaseType;
 import org.zalando.typemapper.core.DatabaseFieldDescriptor;
@@ -36,7 +21,25 @@ import org.zalando.typemapper.core.fieldMapper.AnyTransformer;
 import org.zalando.typemapper.core.fieldMapper.DefaultObjectMapper;
 import org.zalando.typemapper.core.fieldMapper.GlobalValueTransformerRegistry;
 import org.zalando.typemapper.core.fieldMapper.ObjectMapper;
-import org.zalando.sprocwrapper.util.NameUtils;
+
+import java.lang.reflect.Array;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.sql.Types;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class PgTypeHelper {
 
@@ -265,7 +268,9 @@ public class PgTypeHelper {
         for (final Field f : fields) {
             final DatabaseFieldDescriptor databaseFieldDescriptor = getDatabaseFieldDescriptor(f);
             if (databaseFieldDescriptor != null) {
-                f.trySetAccessible();
+                if (!f.isAccessible()) {
+                    f.setAccessible(true);
+                }
 
                 Object value;
                 try {
